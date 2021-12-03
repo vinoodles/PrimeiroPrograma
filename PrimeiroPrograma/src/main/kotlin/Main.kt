bba5e4deaa85f86684a5cd07baddf52547cb66ee
vinoodles <mvcs28@gmail.com>
2021-12-03 14:47:58 -0300

    primeiro

---
+++ PrimeiroPrograma/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println("<???> Opa, eae")
+    println("<Rogério> De boas?")
+    println("<???> Suaves")
+    println("<Rogério> Qual o seu nome?")
+    println("<???> Meu nome é Henrique")
+    println("<Rogério> Opa, prazer em conhecê-lo Henrique")
+    println("O prazer é todo meu")
+}
